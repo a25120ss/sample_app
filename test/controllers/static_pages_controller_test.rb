48ae6125a7fc473d4cc191cf89f41d89ef28ae14
a25120ss <a25120ss@aiit.ac.jp>
2025-05-03 06:52:19 +0000

    Finish Chapter3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do # テスト説明文
+    # GET /static_pages/home
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', 'Home | Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get help" do
+  test 'should get help' do
+    # GET /static_pages/help
     get static_pages_help_url
     assert_response :success
+    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'should get about' do
+    # GET /static_pages/about
+    get static_pages_about_url
+    assert_response :success
+    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
   end
 end
